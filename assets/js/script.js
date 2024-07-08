b73c39f33eac3d4956bb0d2d6d76249ce2d35d64
Seema-476 <simmybishnoi29085@gmail.com>
2024-07-08 14:00:25 +0530

    add down to top btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,6 +75,20 @@ $('.teamresponsive').slick({
     ]
 });
 
+// scroll top btn 
+const topBtn = document.querySelector(".topBtn")
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show")
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
+
     AOS.init({
         duration: 1200,
     once:true
